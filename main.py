60b854f74df36db4740c509bda0138b479a275bd
Gamze Solak <gamzessolakk@gmail.com>
2024-04-28 22:08:51 +0300

    first line added

---
+++ main.py
@@ -0,0 +1,46 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("#424242")
+drawing_board.title("Python Turtle")
+'''
+turtle_instance =turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance_2 =turtle.Turtle()
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+'''
+#Square
+'''
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+'''
+#Loop ile square
+'''turtle_instance =turtle.Turtle()
+for i in range(4):
+    turtle_instance.forward(50)
+    turtle_instance.left(90)
+'''
+#star
+'''turtle_instance = turtle.Turtle()
+for i in range(5) :
+    turtle_instance.forward(100)
+    turtle_instance.right(144)
+'''
+#polygon
+turtle_instance = turtle.Turtle()
+num_sides = 8
+angle = 360.0 / num_sides
+side_length = 100
+
+for i in range(num_sides) :
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_length)
+
+turtle.done()
